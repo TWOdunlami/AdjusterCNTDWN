f4f73edea6ae774892ad26b55f42036096cf0fec
TWOdunlami <t_alvarez88@hotmail.com>
2022-05-21 15:14:40 -0400

    function created for timer countdown. countdown is visible. style for timer not complete.

--- main.js
+++ main.js
@@ -8,5 +8,13 @@ var ramos = setInterval(function() {
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 *60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 *60)) / 1000);
+
+    document.getElementById("tania").innerHTML = days + "d " + hours + "h " 
+    + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(ramos);
+    }
+
 }
     )
